b120ca1956e566254cabd4f49b0ef3e24bf65cc4
dianakius <eingidiana@gmail.com>
2025-10-14 11:31:30 +0300

    Conditional Render the color of the border of the note

--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -9,14 +9,27 @@ const NoteList = ({ notes , deleteNote}) => {
         <div
           key={note.id}
           className="p-4 bg-white rounded-lg shadow-md border-l-4"
+          style={{
+            borderLeftColor:
+              note.priority === 'High'
+                ? 'red'
+                : note.priority === 'Medium'
+                ? 'orange'
+                : 'green',
+          }}
         >
           <h3 className="text-lg font-bold">{note.title}</h3>
           <p className="text-sm text-gray-600">
-            <strong>Category:</strong> {note.category}</p>
+            <strong>Category:</strong> {note.category}
+          </p>
           <p className="text-sm text-gray-600"><strong>Priority:</strong> {note.priority}</p>
           <p className="mt-2 text-gray-800">{note.description}</p>
-
-          <button onClick={() => deleteNote(note.id)}className="mt-3 text-red-500 cursor-pointer transition hover:text-red-700">Delete</button>
+          <button
+            onClick={() => deleteNote(note.id)}
+            className="mt-3 text-red-500 cursor-pointer transition hover:text-red-700"
+          >
+            Delete
+          </button>
         </div>
       ))}
     </div>

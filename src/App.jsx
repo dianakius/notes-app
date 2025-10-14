34ed527b948999c27e5772c03eb78eb7b4664c84
dianakius <eingidiana@gmail.com>
2025-10-14 10:18:10 +0300

    Change the background color of the App

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import NoteList from "./components/NoteList";
 const App = () => {
 const [notes, setNotes] = useState([]);
 
-  return <div className="max-w-lg mx-auto mt-10 p-6 bg-gray-100 rounded-lg shadow-lg">
+  return <div className="max-w-lg mx-auto mt-10 p-6 bg-white rounded-lg shadow-lg">
     <h2 className="text-2xl font-bold mb-4 text-center">Notes App </h2>
 
     <NoteForm notes={notes} setNotes={setNotes}/>
